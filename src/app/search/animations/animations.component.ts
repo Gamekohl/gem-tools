46bc3f740474e4164b44dd9f7fc305d95949d02f
Gamekohl <julian.schwaneberg@t-online.de>
2026-01-26 00:27:07 +0100

    wip

--- src/app/search/animations/animations.component.ts
+++ src/app/search/animations/animations.component.ts
@@ -1,35 +1,27 @@
 import {Clipboard} from "@angular/cdk/clipboard";
-import {AsyncPipe} from '@angular/common';
-import {HttpClient} from '@angular/common/http';
-import {Component, inject, model, OnInit} from '@angular/core';
+import {httpResource} from '@angular/common/http';
+import {ChangeDetectionStrategy, Component, inject, model} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {NgIconComponent, provideIcons} from '@ng-icons/core';
 import {tablerCopy} from '@ng-icons/tabler-icons';
-import {map, Observable} from 'rxjs';
 import {FilterPipe} from '../../pipes/filter.pipe';
 
 @Component({
     selector: 'app-animations',
-    imports: [AsyncPipe, NgIconComponent, FilterPipe, FormsModule],
+    imports: [NgIconComponent, FilterPipe, FormsModule],
     templateUrl: './animations.component.html',
     styleUrl: './animations.component.scss',
-    viewProviders: [provideIcons({tablerCopy})]
+    viewProviders: [provideIcons({tablerCopy})],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AnimationsComponent implements OnInit {
+export class AnimationsComponent {
     private readonly clipboard = inject(Clipboard);
     private readonly snackbar = inject(MatSnackBar);
-    private readonly http = inject(HttpClient);
 
-    animationNames$: Observable<string[]>;
+    animationNames = httpResource<{ names: string[] }>(() => 'assets/animation_names.json');
 
     query = model<string>('');
-
-    ngOnInit(): void {
-        this.animationNames$ = this.http.get<{ names: string[] }>('assets/animation_names.json').pipe(
-            map(({names}) => names)
-        );
-    }
 
     copyAnimationName(name: string): void {
         this.clipboard.copy(name);
