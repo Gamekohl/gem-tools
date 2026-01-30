7e3de550c27f77c8d62f49329a4db6cfbf132753
Gamekohl <julian.schwaneberg@t-online.de>
2026-01-30 12:12:59 +0100

    renamed and updated changelog mds

--- src/content/changelog/changelog.data.ts
+++ src/content/changelog/changelog.data.ts
@@ -1,8 +1,10 @@
-import v000 from './2024-09-12.md';
-import v001 from './2026-01-25.md';
-import v002 from './2026-01-26.md';
-import v003 from './2026-01-27.md';
-import v004 from './2026-01-29.md';
+import v000 from './0.0.0.md';
+import v001 from './0.0.1.md';
+import v002 from './0.0.2.md';
+import v003 from './0.0.3.md';
+import v100 from './1.0.0.md';
+import v110 from './1.1.0.md';
+import v111 from './1.1.1.md';
 
 export interface ChangelogEntry {
     version: string;
@@ -12,7 +14,9 @@ export interface ChangelogEntry {
 }
 
 export const CHANGELOG: ChangelogEntry[] = [
-    { version: '1.0.0', date: '2026-01-29', title: 'Maps and Tutorials', markdown: v004 },
+    { version: '1.1.1', date: '2026-01-30', title: 'Added GSF', markdown: v111 },
+    { version: '1.0.1', date: '2026-01-30', title: 'Fixes and Package Updates', markdown: v110 },
+    { version: '1.0.0', date: '2026-01-29', title: 'Maps and Tutorials', markdown: v100 },
     { version: '0.0.3', date: '2026-01-27', title: 'Data Update', markdown: v003 },
     { version: '0.0.2', date: '2026-01-26', title: 'Preview Pictures', markdown: v002 },
     { version: '0.0.1', date: '2026-01-25', title: 'Package Updates', markdown: v001 },
