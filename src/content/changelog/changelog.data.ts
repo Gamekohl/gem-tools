a2494321800ad92de3fdf84c32b2acef96e451e4
Gamekohl <julian.schwaneberg@t-online.de>
2026-01-30 12:45:01 +0100

    added changelog

--- src/content/changelog/changelog.data.ts
+++ src/content/changelog/changelog.data.ts
@@ -5,6 +5,7 @@ import v003 from './0.0.3.md';
 import v100 from './1.0.0.md';
 import v110 from './1.1.0.md';
 import v111 from './1.1.1.md';
+import v112 from './1.1.2.md';
 
 export interface ChangelogEntry {
     version: string;
@@ -14,6 +15,7 @@ export interface ChangelogEntry {
 }
 
 export const CHANGELOG: ChangelogEntry[] = [
+    { version: '1.1.2', date: '2026-01-30', title: 'Build', markdown: v112 },
     { version: '1.1.1', date: '2026-01-30', title: 'Added GSF', markdown: v111 },
     { version: '1.0.1', date: '2026-01-30', title: 'Fixes and Package Updates', markdown: v110 },
     { version: '1.0.0', date: '2026-01-29', title: 'Maps and Tutorials', markdown: v100 },
